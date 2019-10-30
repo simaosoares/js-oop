ebada9051f6d32d8e1e0f52fda9a1491f409e46a
Simão Soares <s.soares@moliporex.pt>
2019-10-30 11:59:33 +0000

    Added getting the prototype of an object samples.

--- index.js
+++ index.js
@@ -117,5 +117,38 @@ function Circle(radius) {
 const newCircle = new Circle(20);
 console.log(newCircle.getDefaultLocation());
 console.log(newCircle.defaultLocation);
-// will throw exception
-newCircle.defaultLocation = 1;
+// if uncomment the next statement will throw an exception
+// newCircle.defaultLocation = 1;
+
+
+// ///////////////////////////////////////////////////
+// Getting the prototype of an object
+// ///////////////////////////////////////////////////
+// https://tylermcginnis.com/beginners-guide-to-javascript-prototype/
+
+function Animal (name, energy) {
+    this.name = name;
+    this.energy = energy;
+}
+
+Animal.prototype.eat = function (amount) {
+    console.log(`${this.name} is eating.`);
+    this.energy += amount;
+};
+
+Animal.prototype.sleep = function (length) {
+    console.log(`${this.name} is sleeping.`);
+    this.energy += length;
+};
+
+Animal.prototype.play = function (length) {
+    console.log(`${this.name} is playing.`);
+    this.energy -= length;
+};
+
+const leo = new Animal('Leo', 7);
+const prototype = Object.getPrototypeOf(leo);
+
+console.log(prototype); // {constructor: ƒ, eat: ƒ, sleep: ƒ, play: ƒ}
+
+console.log(prototype === Animal.prototype); // true
